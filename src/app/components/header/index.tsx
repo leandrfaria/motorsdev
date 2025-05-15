ea9a22b61b5dcbfe90c6f62474185fb067b1e9ae
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2025-05-14 21:02:51 -0300

    Criando Header

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import styles from './styles.module.scss'
+import Link from 'next/link'
+
+export function Header () {
+
+    const scrollHandler = () => {
+        window.scrollY > 10 ? setTop(false) : setTop(true)
+    }
+
+    const [top,setTop] = useState(true)
+
+    useEffect(() => {
+        const scrollHandler = () => {
+            window.scrollY > 10 ? setTop(false) : setTop(true);
+        };
+
+        window.addEventListener("scroll", scrollHandler);
+        return () => window.removeEventListener("scroll", scrollHandler);
+    }, []);
+
+    return(
+        <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}> 
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.contentLogo}>
+                        <Link href="/">MotorsDev</Link>
+
+                        <nav className={styles.nav}>
+                            <Link href="/">Home</Link>
+
+                            <Link href="/#servicos">Serviços</Link>
+
+                            <Link href="/#contatos">Contatos</Link>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <main>
-      <h1>teste</h1>
+      <div style={{width:"100%", height: 600, backgroundColor: "#121212", marginBottom: 24}}>
+        <h1>teste</h1>
+      </div>
+      <div style={{width:"100%", height: 600, backgroundColor: "blue", marginBottom: 24}}>
+        <h1>teste</h1>
+      </div>
+      <div style={{width:"100%", height: 600, backgroundColor: "red", marginBottom: 24}}>
+        <h1>teste</h1>
+      </div>
+  
     </main>
   );
 }
